3d553088de7d6564149ae52a040813024fb31916
Nana Mohamadi <adilnana78@gmail.com>
2024-11-17 14:49:55 +0100

    Complete the app overview inside the admin dashboard

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -74,7 +74,7 @@ export async function POST(req) {
           await clerkClient.users.updateUserMetadata(id, {
             publicMetadata: {
               userMongoId: user._id,
-              isAdmin: user.isAdmin,
+              isAdmin: user.isAdmin || false,
             },
           });
         } catch (error) {
